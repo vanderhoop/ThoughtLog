8e72730cb167d68501c2146b2b67ab7359a9d6dd
Travis Vander Hoop <vanderhoop@me.com>
2013-10-23 19:07:18 -0400

    edits to ideas are now persisted

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,7 +7,12 @@ class IdeasController < ApplicationController
   end
 
   def update
-    raise
+    @idea = Idea.find(params[:id])
+    if @idea.update_attributes(params[:idea])
+      redirect_to "/users/#{session[:user_id]}/entries/#{params[:entry_id]}"
+    else
+      render :edit
+    end
   end
 
 end
