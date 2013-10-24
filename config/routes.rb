7f9b0143aa38a59af3b6d3d55a6cf7e48cf11305
Travis Vander Hoop <vanderhoop@me.com>
2013-10-24 14:04:07 -0400

    deleted uneeded spacing on create forms

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ThoughtLogApp::Application.routes.draw do
   post '/logout' => 'session#destroy'
   get '/' => 'session#new'
   get '/users/:user_id/entries/:entry_id/ideas/:idea_id/edit' => 'ideas#edit'
-  get 'users/:user_id/insights' => 'users#insight'
+  get '/users/:user_id/insights' => 'users#insight'
 
 
   get '/users/:id/dashboard' => 'users#dashboard'
