08cf69abd5033e7693e33581b93a99eba684515d
Travis Vander Hoop <vanderhoop@me.com>
2013-10-27 13:52:47 -0400

    took out all unecessary declarations of @user=User.find(params[:user_id]), owing to its inclusion in the application controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,8 +7,7 @@ class EntriesController < ApplicationController
   end
 
   def show
-    user = User.find(params[:user_id])
-    @entry_ideas = user.ideas.where(entry_id: params[:id])
+    @entry_ideas = @user.ideas.where(entry_id: params[:id])
     @entry = Entry.find(params[:id])
   end
 
@@ -16,7 +15,6 @@ class EntriesController < ApplicationController
     # render entries/new entry form
     @prompts = ["Think of a time you learned something from or came to an
 agreement with someone you disliked.", "What would it be like to have yourself as a spouse?", "What would you say to your 16-year old self if you couldn't explain who you were?", "If you had no restrictions whatsoever, what 9-to-5 job would you have?", "Would you say routine makes your job better or worse?", "What surprised you about today? It can be anything at all, even a nice hello.", "Think of something you spent weeks anticipating only to be disappointed. How do you feel about that disappointment now?", "If you had to give the sky a new color, what color would you choose? And who does this color remind you of?", "Write about your family. Whatever comes to mind."]
-    @user = User.find(params[:user_id])
     @entry = @user.entries.build
   end
 
@@ -47,14 +45,12 @@ agreement with someone you disliked.", "What would it be like to have yourself a
   end
 
   def edit
-    @user = User.find(params[:user_id])
     @entry = @user.entries.build              #my categorie and :emotional_weight edits aren't persisted in the database. Why is that?
   end
 
   def destroy
     #I want to destroy all entries associated with this entry.
     #This code works, but I used a 'link_to' in the view, when Stack Overflowers recommend using a 'button_to'. Link_to defaults to a 'GET' method, while button_to defaults to a 'POST' method.
-    @user = User.find(params[:user_id])
     @entry_ideas = @user.ideas.where(entry_id: params[:id])
     @entry_ideas.each { |idea| idea.destroy }
     @entry = Entry.find(params[:id])
